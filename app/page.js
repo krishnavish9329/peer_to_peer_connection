3541f35c31eb703ef50825d6a1e0b7d4f980f7eb
Krishna Kumar Vishwakarma <108523587+krishnavish9329@users.noreply.github.com>
2026-06-09 23:37:00 +0530

    crete the dummy page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>welcom to peer to peer connection</h1>
+      <p>we can share data between two devices, with we can share data into  Computer to mobail also and vice versa</p>
+      <Link href="/peer-to-peer">Peer to Peer</Link>
     </div>
   );
 }
