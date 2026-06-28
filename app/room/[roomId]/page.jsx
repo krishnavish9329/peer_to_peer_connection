73bc33baf44a3c16d5006ba13249e3be8472fa70
krishna2378 <krishna.kumar@htinfosystem.com>
2026-06-28 23:02:20 +0530

    add :

--- app/room/[roomId]/page.jsx
+++ app/room/[roomId]/page.jsx
@@ -1,45 +1,32 @@
 "use client";
-
 import { use } from "react";
 import { useState } from "react";
 import useWebSocket from "@/hooks/useWebSocket";
-
 export default function Room({ params }) {
   const { roomId } = use(params);
-
   const {
     peerId,
     peers,
     messages,
     sendMessage,
   } = useWebSocket(roomId);
-
   const [text, setText] = useState("");
-
   return (
     <div style={{ padding: 30 }}>
       <h1>Room : {roomId}</h1>
-
       <h3>Your Peer ID</h3>
-
       <p>{peerId}</p>
-
       <hr />
-
       <h3>Connected Peers</h3>
-
       {peers.map((peer) => (
         <div key={peer}>{peer}</div>
       ))}
-
       <hr />
-
       <input
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="Type message..."
       />
-
       <button
         onClick={() => {
           sendMessage(text);
@@ -48,19 +35,13 @@ export default function Room({ params }) {
       >
         Send
       </button>
-
       <hr />
-
       <h3>Messages</h3>
-
       {messages.map((msg, index) => (
         <div key={index}>
           <strong>{msg.fromPeerId}</strong>
-
           <br />
-
           {msg.message}
-
           <hr />
         </div>
       ))}
